6ae81e955e0e81f3e407536ccca54b4cffb96e18
Eunjae Lee <hey@eunjae.dev>
2023-05-21 16:11:11 +0200

    update

--- commands/always/connect-bluetooth-devices.sh
+++ commands/always/connect-bluetooth-devices.sh
@@ -2,11 +2,14 @@
 
 # Required parameters:
 # @raycast.schemaVersion 1
-# @raycast.title Connect apple keyboard and trackpad
+# @raycast.title Re-connect apple keyboard and trackpad
 # @raycast.mode compact
 
 # Optional parameters:
 # @raycast.icon ⌨️
 
-blueutil --connect b0-be-83-f1-90-b4 &
+blueutil --unpair b0-be-83-f1-90-b4
+blueutil --unpair 14-c2-13-ee-5e-21
+
+blueutil --connect b0-be-83-f1-90-b4
 blueutil --connect 14-c2-13-ee-5e-21
